f7e7c4095dd9ae2ea3e27923da36e41299b4685c
Gjermundhp02 <gjermund.pedersen@gmail.com>
2023-06-15 12:32:13 +0200

    Created project and added nav bar

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <NuxtLink to="/" id="title">Turtleman</NuxtLink>
+    <NuxtLink class="right" to="/Projects" >Projects</NuxtLink>
+    <NuxtLink class="right" to="/Projects">Login</NuxtLink>
+  </nav>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+  body{
+    margin: 0;
+    font-family: sans-serif;
+  }
+  nav{
+    background-color: gray;
+    padding: 1svw;
+    & a:not(:first-child){
+      padding-left: 1svw;
+    }
+  }
+  a{
+    color: black;
+    text-decoration: none;
+  }
+  .right{
+    float: right;
+  }
+  #title{
+    font-weight: bold;
+  }
+</style>
